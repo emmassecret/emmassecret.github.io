fa363be6b0c920b26e2b79286e152fcf81fb3d9b
emmassecret <slcutahemma@gmail.com>
2024-11-09 15:32:03 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+setup();
+
+function setup() {
+  let text = d3.select("content").append("text").text("Welcome to my secret bio page! If you found this page on your own I'm impressed. If I directed you here it's because I wanted to give you a little more information about me because messaging is really the worst and I don't have an instagram.");
+}
